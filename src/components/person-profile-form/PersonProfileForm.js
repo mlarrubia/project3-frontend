deff465504788f23bd098ca3101823e54f9c399a
Matthew Larrubia <mlarrubia@columbushs.com>
2019-08-01 19:30:54 -0400

    added components

--- src/components/person-profile-form/PersonProfileForm.js
+++ src/components/person-profile-form/PersonProfileForm.js
@@ -139,9 +139,9 @@ class PersonProfileForm extends React.Component {
 
 
                 {/* This does not work */}
-                {/* <a class="twitter-timeline" href="https://twitter.com/amandajimenezzz?ref_src=twsrc%5Etfw">Tweets by cryptoWZRD_</a> 
+                <a class="twitter-timeline" href="https://twitter.com/amandajimenezzz?ref_src=twsrc%5Etfw">Tweets by cryptoWZRD_</a> 
                 <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script> 
-                 */}
+                
                 <blockquote class="twitter-tweet"><p lang="en" dir="ltr">Sunsets don&#39;t get much better than this one over <a href="https://twitter.com/GrandTetonNPS?ref_src=twsrc%5Etfw">@GrandTetonNPS</a>. <a href="https://twitter.com/hashtag/nature?src=hash&amp;ref_src=twsrc%5Etfw">#nature</a> <a href="https://twitter.com/hashtag/sunset?src=hash&amp;ref_src=twsrc%5Etfw">#sunset</a> <a href="http://t.co/YuKy2rcjyU">pic.twitter.com/YuKy2rcjyU</a></p>&mdash; US Department of the Interior (@Interior) <a href="https://twitter.com/Interior/status/463440424141459456?ref_src=twsrc%5Etfw">May 5, 2014</a></blockquote> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
 
                 <TwitterTweetEmbed
